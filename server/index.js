8fd28f67ebd898bafccc5eb4352866f4467f1e0b
exerex <exerex11@gmail.com>
2020-04-12 12:52:22 -0300

    add express and socket connect and disconect functions

--- server/index.js
+++ server/index.js
@@ -5,9 +5,24 @@ const http = require('http')
 //           para deployado
 const PORT = process.env.PORT || 5000
 
+//importamos el router
+const router = require('./router')
+
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('Tenemos una nueva conexion')
+
+    socket.on('disconnect', () => {
+        console.log('Se ha terminado la conexion');
+    })
+})
+
+//lo importamos como un middleware
+app.use(router)
 
 //especificamos el puerto
 server.listen(PORT, ()=>{
