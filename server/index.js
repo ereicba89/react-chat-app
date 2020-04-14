84c860d061d866f56c963c4cee9fac9965d103d1
exerex <exerex11@gmail.com>
2020-04-14 11:07:20 -0300

    add navbar, messages, message components

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 
+//importando el modulo de usuarios
+const { addUser, removeUser, getUser, getUserInRoom } = require('./users.js')
+
 //           para deployado
 const PORT = process.env.PORT || 5000
 
@@ -13,8 +16,30 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
+io.on('connect', (socket) => {
     console.log('Tenemos una nueva conexion')
+
+    socket.on('ingresando', ({name, room}, callback) => {
+        const { error, user } = addUser({id: socket.id, name, room})
+        if(error){
+            return callback(error)
+        }
+        socket.emit('mensaje', { user: 'admin', text: `${user.name}, bienvenido a la sala ${user.room}`})
+        socket.broadcast.to(user.room).emit('mensaje', { user: 'admin', text: `${user.name} ha ingresado`})
+
+        socket.join(user.room);
+
+        callback()
+    })
+
+    socket.on('enviarMensaje', (mensaje, callback) => {
+        const user = getUser(socket.id);
+        console.log(user);
+
+        io.to(user.room).emit('mensaje', {user: user.name, text: mensaje})
+
+        callback()
+    })
 
     socket.on('disconnect', () => {
         console.log('Se ha terminado la conexion');
